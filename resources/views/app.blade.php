7c28a84857da84c8118f98b1cd40e4026abd2ad0
Brent Deacon <brent.deacon@ourvacationcentre.com>
2015-08-25 09:20:56 +1000

    Try

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -67,16 +67,6 @@
 	<![endif]-->
 </head>
 <body>
-	<!--=== Recent Posts ===-->    
-	<div id="fb-root"></div>
-	<script>(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&appId=1428829950709319&version=v2.0";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script>
-	<!--=== End Recent Posts ===--> 
 	<!--=== Top ===-->    
 	<div class="top">
 	    <div class="container">         
