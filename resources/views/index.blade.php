83f4aa2106c1e50237f2f9783f80bbdcb4a0ac8d
Brent Deacon <brent.deacon@ourvacationcentre.com>
2015-08-25 11:14:54 +1000

    Fix slider

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -187,14 +187,41 @@
 	<script src="{{ asset('/js/responsive-slider.min.js') }}"></script>
 	<script type="text/javascript" src="{{ asset('/plugins/flexslider/jquery.flexslider-min.js') }}"></script>
 	<script type="text/javascript">
-	    $(window).load(function() {
-		  $('.flexslider').flexslider({
-		    animation: "slide",
-		    animationLoop: true,
-		    itemWidth: 210,
-		    itemMargin: 5
+	    (function() {
+ 
+		  // store the slider in a local variable
+		  var $window = $(window),
+		      flexslider;
+		 
+		  // tiny helper function to add breakpoints
+		  function getGridSize() {
+		    return (window.innerWidth < 600) ? 2 :
+		           (window.innerWidth < 900) ? 3 : 4;
+		  }
+		 
+		  $(function() {
+		    SyntaxHighlighter.all();
 		  });
-		});
+		 
+		  $window.load(function() {
+		    $('.flexslider').flexslider({
+		      animation: "slide",
+		      animationLoop: false,
+		      itemWidth: 210,
+		      itemMargin: 5,
+		      minItems: getGridSize(), // use function to pull in initial value
+		      maxItems: getGridSize() // use function to pull in initial value
+		    });
+		  });
+		 
+		  // check grid size on resize event
+		  $window.resize(function() {
+		    var gridSize = getGridSize();
+		 
+		    flexslider.vars.minItems = gridSize;
+		    flexslider.vars.maxItems = gridSize;
+		  });
+		}());
 	</script>
 	<script>	
 		$(document).ready(function(){	  
