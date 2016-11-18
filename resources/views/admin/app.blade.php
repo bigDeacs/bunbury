c4f2f873bf0d505b385992765905e192ed3428bf
bigDeacs <brentdeacon23@gmail.com>
2016-11-18 13:49:48 +1000

    Add Gallery

--- resources/views/admin/app.blade.php
+++ resources/views/admin/app.blade.php
@@ -74,10 +74,10 @@
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> Gallery <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('/home/galleries') }}"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> Award Gallery</a></li>
-								<li><a href="{{ url('/home/galleryrankings') }}"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Award Gallery Ranking</a></li>
+								<li><a href="{{ url('/home/galleries') }}"><span class="glyphicon glyphicon-picture" aria-hidden="true"></span> Showcase</a></li>
+								<li><a href="{{ url('/home/galleryrankings') }}"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> Showcase Ranking</a></li>
 								<li class="divider"></li>
-								<li><a href="{{ url('/home/imagelabels') }}"><span class="glyphicon glyphicon-tag" aria-hidden="true"></span> Award Gallery Labels</a></li>
+								<li><a href="{{ url('/home/imagelabels') }}"><span class="glyphicon glyphicon-tag" aria-hidden="true"></span> Showcase Labels</a></li>
 							</ul>
 						</li>
 						<li class="dropdown">
