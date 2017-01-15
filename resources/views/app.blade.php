0f877e8732fea54d30bdb7d277b042dea1f9f2ff
bigDeacs <brentdeacon23@gmail.com>
2017-01-16 09:53:26 +1000

    Fix offers

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -65,11 +65,7 @@
 		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
-	<script src='https://embed.tawk.to/55f644695cc4b66b4008cc99/default' charset="UTF-8" crossorigin="*"></script>
-	<script type="text/javascript" src="https://www.gstatic.com/recaptcha/api2/r20170109113303/recaptcha__en.js"></script>
 	<script src='https://www.google.com/recaptcha/api.js'></script>
-	<style>.grecaptcha-badge:hover { right: 4px !important }</style>
-	
 </head>
 <body>
 	<!--=== Top ===-->
